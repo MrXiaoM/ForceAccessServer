f75174da06b379b75c232cc0d29630fdb309804c
MrXiaoM <coolxiaom95@gmail.com>
2023-09-03 16:06:20 +0800

    ignore mixin fail

--- src/main/java/top/mrxiaom/fas/mixin/MixinOfflineSocialInteractions.java
+++ src/main/java/top/mrxiaom/fas/mixin/MixinOfflineSocialInteractions.java
@@ -4,7 +4,9 @@ import com.mojang.authlib.minecraft.OfflineSocialInteractions;
 import com.mojang.authlib.yggdrasil.YggdrasilSocialInteractionsService;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
 
+@Pseudo
 @Mixin(OfflineSocialInteractions.class)
 public abstract class MixinOfflineSocialInteractions {
 
--- src/main/java/top/mrxiaom/fas/mixin/MixinYggdrasilSocialInteractionsService.java
+++ src/main/java/top/mrxiaom/fas/mixin/MixinYggdrasilSocialInteractionsService.java
@@ -3,10 +3,12 @@ package top.mrxiaom.fas.mixin;
 import com.mojang.authlib.yggdrasil.YggdrasilSocialInteractionsService;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Map;
 
+@Pseudo
 @Mixin(YggdrasilSocialInteractionsService.class)
 public abstract class MixinYggdrasilSocialInteractionsService {
 
