df56341fddff545a3ad0203cab640306a5479f52
MrXiaoM <coolxiaom95@gmail.com>
2023-09-03 19:37:26 +0800

    prevent Offline properties change

--- src/main/java/top/mrxiaom/fas/mixin/MixinYggdrasilUserApiService.java
+++ src/main/java/top/mrxiaom/fas/mixin/MixinYggdrasilUserApiService.java
@@ -3,8 +3,13 @@ package top.mrxiaom.fas.mixin;
 import com.mojang.authlib.exceptions.AuthenticationException;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import top.mrxiaom.fas.FabricMod;
 
+import java.lang.reflect.Field;
+
+@Pseudo
 @Mixin(targets = { "com.mojang.authlib.yggdrasil.YggdrasilUserApiService" })
 public class MixinYggdrasilUserApiService {
     /**
@@ -14,6 +19,16 @@ public class MixinYggdrasilUserApiService {
      */
     @Overwrite
     private void fetchProperties() throws AuthenticationException {
-        FabricMod.LOGGER.info("已阻止拉取配置，默认使用离线配置");
+        FabricMod.LOGGER.info("已阻止拉取配置");
+        // 懒得导包，而且用java17编译包也导不进来，直接反射
+        try {
+            Object allow = Class.forName("top.mrxiaom.fas.UnlimitedSocialInteractions3").getDeclaredField("ALLOW").get(null);
+            Field properties = this.getClass().getDeclaredField("properties");
+            properties.setAccessible(true);
+            properties.set(this, allow);
+            FabricMod.LOGGER.info("成功更改配置");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
